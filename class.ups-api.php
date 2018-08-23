3ae729141e857a093beabaa77e790fe07d4f7a93
Jasper Carpizo <jcarpizo.it@gmail.com>
2018-08-23 21:25:34 +0800

    updated files

--- class.ups-api.php
+++ class.ups-api.php
@@ -3,9 +3,10 @@
 class UpsApi extends WP_REST_Controller
 {
 
-    const ACCESS_KEY = null;
-    const USER_ID = null;
-    const PASSWORD = null;
+    const ACCESS_KEY = '7D4D06F2836E47ED';
+    const USER_ID = 'BigUglyApe';
+    const PASSWORD = 'DigiApe2020!!';
+    const SHIPPER_NO = '149V7W';
 
     public function init()
     {
@@ -40,70 +41,67 @@ class UpsApi extends WP_REST_Controller
     public function post_shipment_details($request)
     {
 
-        $requestData = $request->get_params();
-
         $shipment = new Ups\Entity\Shipment;
         $shipper = $shipment->getShipper();
-        $shipper->setShipperNumber($requestData->shipper_number);
-        $shipper->setName($requestData->shipment_name);
-        $shipper->setAttentionName($requestData->shipment_attention_name);
+        $shipper->setShipperNumber(self::SHIPPER_NO);
+        $shipper->setName($request['shipment_name']);
+        $shipper->setAttentionName($request['shipment_attention_name']);
         $shipperAddress = $shipper->getAddress();
-        $shipperAddress->setAddressLine1($requestData->shipment_address);
-        $shipperAddress->setPostalCode($requestData->shipment_postal_code);
-        $shipperAddress->setCity($requestData->city);
-        $shipperAddress->setStateProvinceCode($requestData->shipment_province_code); // required in US
-        $shipperAddress->setCountryCode($requestData->shipment_country_code);
+        $shipperAddress->setAddressLine1($request['shipment_address']);
+        $shipperAddress->setPostalCode($request['shipment_postal_code']);
+        $shipperAddress->setCity($request['shipment_city']);
+        $shipperAddress->setStateProvinceCode($request['shipment_province_code']); // required in US
+        $shipperAddress->setCountryCode($request['shipment_country_code']);
         $shipper->setAddress($shipperAddress);
-        $shipper->setEmailAddress($requestData->shipment_email_address);
-        $shipper->setPhoneNumber($requestData->shipment_phone_number);
+        $shipper->setEmailAddress($request['shipment_email_address']);
+        $shipper->setPhoneNumber($request['shipment_phone_number']);
         $shipment->setShipper($shipper);
+
+        // From address
+        $address = new \Ups\Entity\Address();
+        $address->setAddressLine1($shipperAddress->getAddressLine1());
+        $address->setPostalCode($shipperAddress->getPostalCode());
+        $address->setCity($shipperAddress->getCity());
+        $address->setStateProvinceCode($shipperAddress->getStateProvinceCode());
+        $address->setCountryCode($shipperAddress->getCountryCode());
+        $shipFrom = new \Ups\Entity\ShipFrom();
+        $shipFrom->setAddress($address);
+        $shipFrom->setName( $shipper->getAttentionName());
+        $shipFrom->setAttentionName($shipper->getAttentionName());
+        $shipFrom->setCompanyName($shipper->getAttentionName());
+        $shipFrom->setEmailAddress($shipper->getEmailAddress());
+        $shipFrom->setPhoneNumber($shipper->getPhoneNumber());
+        $shipment->setShipFrom($shipFrom);
 
 
         // To address
         $address = new \Ups\Entity\Address();
-        $address->setAddressLine1($requestData->toaddress_one);
-        $address->setPostalCode($requestData->toaddress_postal_code);
-        $address->setCity($requestData->toaddress_city);
-        $address->setStateProvinceCode($requestData->toaddress_province_code);  // Required in US
-        $address->setCountryCode($requestData->toaddress_country_code);
+        $address->setAddressLine1($request['toaddress_one']);
+        $address->setPostalCode($request['toaddress_postal_code']);
+        $address->setCity($request['toaddress_city']);
+        $address->setStateProvinceCode($request['toaddress_province_code']);  // Required in US
+        $address->setCountryCode($request['toaddress_country_code']);
         $shipTo = new \Ups\Entity\ShipTo();
         $shipTo->setAddress($address);
-        $shipTo->setCompanyName($requestData->tocompany_name);
-        $shipTo->setAttentionName($requestData->toaddress_attention_name);
-        $shipTo->setEmailAddress($requestData->toaddress_email);
-        $shipTo->setPhoneNumber($requestData->toaddress_phone_number);
+        $shipTo->setCompanyName($request['tocompany_name']);
+        $shipTo->setAttentionName($request['toaddress_attention_name']);
+        $shipTo->setEmailAddress($request['toaddress_email']);
+        $shipTo->setPhoneNumber($request['toaddress_phone_number']);
         $shipment->setShipTo($shipTo);
 
-
-        // From address
-        $address = new \Ups\Entity\Address();
-        $address->setAddressLine1($requestData->fromaddress_one);
-        $address->setPostalCode($requestData->fromaddress_postal_code);
-        $address->setCity($requestData->fromaddress_city);
-        $address->setStateProvinceCode($requestData->fromaddress_province_code);
-        $address->setCountryCode($requestData->fromaddress_country_code);
-        $shipFrom = new \Ups\Entity\ShipFrom();
-        $shipFrom->setAddress($address);
-        $shipFrom->setName($requestData->fromcompany_name);
-        $shipFrom->setAttentionName($shipFrom->getName());
-        $shipFrom->setCompanyName($shipFrom->getName());
-        $shipFrom->setEmailAddress($requestData->fromaddress_email);
-        $shipFrom->setPhoneNumber($requestData->fromaddress_phone_number);
-        $shipment->setShipFrom($shipFrom);
-
         // Sold to
-        $address = new \Ups\Entity\Address();
-        $address->setAddressLine1($requestData->soldaddress_one);
-        $address->setPostalCode($requestData->soldaddress_postal_code);
-        $address->setCity($requestData->soldaddress_city);
-        $address->setCountryCode($requestData->soldaddress_country_code);
-        $address->setStateProvinceCode($requestData->soldaddress_province_code);
+        $addressSold = new \Ups\Entity\Address();
+        $addressSold->setAddressLine1($address->getAddressLine1());
+        $addressSold->setPostalCode($address->getPostalCode());
+        $addressSold->setCity($address->getCity());
+        $addressSold->setCountryCode($address->getCountryCode());
+        $addressSold->setStateProvinceCode($address->getStateProvinceCode());
         $soldTo = new \Ups\Entity\SoldTo;
-        $soldTo->setAddress($address);
-        $soldTo->setAttentionName($requestData->soldaddress_attention_name);
-        $soldTo->setCompanyName($soldTo->getAttentionName());
-        $soldTo->setEmailAddress($requestData->soldaddress_email);
-        $soldTo->setPhoneNumber($requestData->soldaddress_phone_number);
+        $soldTo->setAddress($addressSold);
+        $soldTo->setAttentionName($shipTo->getAttentionName());
+        $soldTo->setCompanyName($shipTo->getCompanyName());
+        $soldTo->setEmailAddress($shipTo->getEmailAddress());
+        $soldTo->setPhoneNumber($shipTo->getPhoneNumber());
         $shipment->setSoldTo($soldTo);
 
         // Set service
@@ -120,21 +118,21 @@ class UpsApi extends WP_REST_Controller
         }
 
         // Set description
-        $shipment->setDescription('XX');
+        $shipment->setDescription('test');
 
         // Add Package
         $package = new \Ups\Entity\Package();
         $package->getPackagingType()->setCode(\Ups\Entity\PackagingType::PT_PACKAGE);
-        $package->getPackageWeight()->setWeight(10);
+        $package->getPackageWeight()->setWeight(1);
         $unit = new \Ups\Entity\UnitOfMeasurement;
         $unit->setCode(\Ups\Entity\UnitOfMeasurement::UOM_KGS);
         $package->getPackageWeight()->setUnitOfMeasurement($unit);
 
         // Set dimensions
         $dimensions = new \Ups\Entity\Dimensions();
-        $dimensions->setHeight(50);
-        $dimensions->setWidth(50);
-        $dimensions->setLength(50);
+        $dimensions->setHeight(1);
+        $dimensions->setWidth(1);
+        $dimensions->setLength(1);
         $unit = new \Ups\Entity\UnitOfMeasurement;
         $unit->setCode(\Ups\Entity\UnitOfMeasurement::UOM_CM);
         $dimensions->setUnitOfMeasurement($unit);
@@ -146,19 +144,8 @@ class UpsApi extends WP_REST_Controller
         // Add this package
         $shipment->addPackage($package);
 
-        // Set Reference Number
-        $referenceNumber = new \Ups\Entity\ReferenceNumber;
-        if ($return) {
-            $referenceNumber->setCode(\Ups\Entity\ReferenceNumber::CODE_RETURN_AUTHORIZATION_NUMBER);
-            $referenceNumber->setValue($return_id);
-        } else {
-            $referenceNumber->setCode(\Ups\Entity\ReferenceNumber::CODE_INVOICE_NUMBER);
-            $referenceNumber->setValue($order_id);
-        }
-        $shipment->setReferenceNumber($referenceNumber);
-
-        // Set payment information
-        $shipment->setPaymentInformation(new \Ups\Entity\PaymentInformation('prepaid', (object)array('AccountNumber' => 'XX')));
+        $shipment->setPaymentInformation(new \Ups\Entity\PaymentInformation('prepaid',
+            (object)array('AccountNumber' => self::SHIPPER_NO)));
 
         // Ask for negotiated rates (optional)
         $rateInformation = new \Ups\Entity\RateInformation;
@@ -170,14 +157,17 @@ class UpsApi extends WP_REST_Controller
             $api = new Ups\Shipping(self::ACCESS_KEY, self::USER_ID, self::PASSWORD);
 
             $confirm = $api->confirm(\Ups\Shipping::REQ_VALIDATE, $shipment);
-            var_dump($confirm); // Confirm holds the digest you need to accept the result
+            //var_dump($confirm); // Confirm holds the digest you need to accept the result
 
             if ($confirm) {
                 $accept = $api->accept($confirm->ShipmentDigest);
-                return new WP_REST_Response($accept, 200);
-                // var_dump($accept); // Accept holds the label and additional information
-            }
+                //return new WP_REST_Response($accept, 200);
+                 var_dump($accept); // Accept holds the label and additional information
+            }//
+
         } catch (\Exception $e) {
+
+          //  var_dump($order_id);
             $message = [
                 'error_message' => $e->getMessage(),
                 'error_code' => $e->getCode()
